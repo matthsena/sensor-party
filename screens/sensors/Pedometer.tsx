1080f23df03c3d1a1940c9211bd17631b607f49d
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-18 22:33:36 -0300

    chore: obtendo passos realizados no dia

--- screens/sensors/Pedometer.tsx
+++ screens/sensors/Pedometer.tsx
@@ -6,6 +6,7 @@ export default function PedometerScreen() {
   const subscription = useRef<Pedometer.Subscription>()
 
   const [state, setState] = useState('checking')
+  const [pastSteps, setPastSteps] = useState<number>()
   const [steps, setSteps] = useState<number>(0)
 
   useEffect(() => {
@@ -22,27 +23,44 @@ export default function PedometerScreen() {
       console.log('step', r.steps)
       setSteps(r.steps)
     })
+
+    Pedometer.isAvailableAsync().then(
+      result => {
+        setState(String(result));
+      },
+      error => {
+        setState('Could not get isPedometerAvailable: ' + error);
+      }
+    );
+
+    const end = new Date();
+    const start = new Date();
+
+    start.setDate(end.getDate() - 1);
+
+    Pedometer.getStepCountAsync(start, end).then(
+      result => {
+        setPastSteps(result.steps);
+      },
+      error => {
+        throw new Error(error);
+      }
+    );
   }
 
   const PedometerUnsub = () => {
     subscription.current?.remove()
   }
 
-  Pedometer.isAvailableAsync().then(
-    result => {
-      setState(String(result));
-    },
-    error => {
-      setState('Could not get isPedometerAvailable: ' + error);
-    }
-  );
 
 
   return (
     <View style={styles.container}>
       <Text>Pedômetro está disponível: {state}</Text>
 
-      <Text>Passos: {steps}</Text>
+      <Text>Passos dados no dia: {pastSteps}</Text>
+
+      <Text>Passos atual: {steps}</Text>
     </View>
   )
 }
