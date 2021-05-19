6e4506b209c4d2a05e353a069bd7f61959fef771
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-18 21:34:00 -0300

    chore: obtendo numero de passos com pedometro

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function Home() {
+export default function Home(props: any) {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate('Pedometer')}>
+        <Text>Pedometro</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,12 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding: 16,
+    backgroundColor: '#ffbb00',
+    marginVertical: 16,
+  }
 });
--- screens/sensors/Pedometer.tsx
+++ screens/sensors/Pedometer.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useRef, useEffect } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Pedometer } from 'expo-sensors'
+
+export default function PedometerScreen() {
+
+  const [state, setState] = useState('checking')
+
+  const [steps, setSteps] = useState<number>(0)
+
+  useEffect(() => {
+    PedometerSub()
+  }, [])
 
 
-export default function Pedometer() {
+  const PedometerSub = () => {
+    Pedometer.watchStepCount(r => {
+      console.log('step', r.steps)
+      setSteps(r.steps)
+    })
+  }
+
+  Pedometer.isAvailableAsync().then(
+    result => {
+      setState(String(result));
+    },
+    error => {
+      setState('Could not get isPedometerAvailable: ' + error);
+    }
+  );
+
+
   return (
-    <View>
-
+    <View style={styles.container}>
+      <Text>Passos: {steps}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
