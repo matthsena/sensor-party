31804686e114b36e79e3bc418b0a1135bf3dfe68
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-26 16:25:23 -0300

    feat: barômetro

--- src/screens/sensors/Barometer.tsx
+++ src/screens/sensors/Barometer.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity, Dimensions } from 'react-native'
 import { Barometer } from 'expo-sensors'
 import { Subscription } from 'expo-sensors/build/Pedometer';
 import { Navigator } from '../../../routes'
-import { Container } from '../../components/Styled';
+import { Container, DefaultCard, LottieContainer, Row } from '../../components/Styled';
+import LottieView from 'lottie-react-native'
+
+const { width, height } = Dimensions.get('window')
 
 export default function BarometerScreen({ navigation }: { navigation: Navigator }) {
   const subscription = useRef<Subscription>()
@@ -49,23 +52,70 @@ export default function BarometerScreen({ navigation }: { navigation: Navigator 
 
   return (
     <Container>
-      <Text>Pressure: {pressure * 100} Pa</Text>
-      <Text>
-        Relative Altitude:{' '}
-        {Platform.OS === 'ios' ? `${relativeAltitude} m` : `Only available on iOS`}
-      </Text>
+      <DefaultCard style={styles.shadow}>
+        <TouchableOpacity style={styles.textWrapper} onPress={() => onReset()}>
+          <Text style={styles.text}>
+            <Text style={styles.textAxis}>
+              Pressão:
+            </Text>
+            {' '}{((pressure * 100) / 101325).toFixed(2)} Atmosfera
+          </Text>
 
-      <TouchableOpacity style={styles.button} onPress={() => onReset()}>
-        <Text>Resetar</Text>
-      </TouchableOpacity>
+          <Text style={styles.text}>
+            <Text style={styles.textAxis}>
+              H relativo:
+            </Text>
+            {' '}  {Platform.OS === 'ios' ? `${relativeAltitude?.toFixed(2)} m` : `Apenas iOS`}
+          </Text>
+        </TouchableOpacity>
+      </DefaultCard>
+
+      <Row>
+        <LottieContainer style={{ ...styles.shadow, ...styles.secondaryCard }}>
+          <LottieView
+            source={require('../../lottiefiles/space.json')}
+            autoPlay
+            loop
+          />
+        </LottieContainer>
+      </Row>
     </Container>
   )
 }
 
 const styles = StyleSheet.create({
-  button: {
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 1.5,
+    elevation: 2,
+  },
+  container: {
     padding: 16,
-    backgroundColor: '#ffbb00',
-    marginVertical: 16,
-  }
+    height: height * 0.4,
+    width: width - 32,
+    backgroundColor: '#eee',
+    borderRadius: 32
+  },
+  textWrapper: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 18,
+    marginTop: 16,
+    color: '#73DB2E'
+  },
+  textAxis: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    fontStyle: 'italic',
+  },
+  secondaryCard: {
+    backgroundColor: '#73DB2E'
+  },
 })
